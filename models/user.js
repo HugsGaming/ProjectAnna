eb4c9aca8d8a78ef7f4e6ab1b2d419e90935c3d4
HugsGaming <jovenreyes020803@gmail.com>
2021-08-02 17:44:42 +0800

    Add User Model with LogIn

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [(val) => {
+            const regex = /\w+@iacademy.edu.ph/g;
+            return regex.test(val);
+        }, 'Enter a valid iAcademy email address']
+    },
+    passwords: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.statics.logIn = async function(email, password) {
+    const user = await this.findOne({email});
+    if(user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if(auth) {
+            return user;
+        }
+        throw Error('Incorrect Password');
+    }
+    throw Error('Incorrect Email')
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
