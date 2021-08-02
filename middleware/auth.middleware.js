d677d7f80669a875e1acf69cd5b8f5f7ab3b3129
HugsGaming <jovenreyes020803@gmail.com>
2021-08-02 15:05:08 +0800

    Add a AuthMiddleware and Rename Create file

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if(token) {
+        jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
+            if(err) {
+                console.error(err);
+                res.redirect('/login');
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+}
